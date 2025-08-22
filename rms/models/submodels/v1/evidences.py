798d1f557f8fbd8f17461abe9624d1aa20e0a46a
Luis <gerardo.fosado@cargamos.com>
2025-08-22 12:41:59 -0600

    fix: error update model file_type

--- rms/models/submodels/v1/evidences.py
+++ rms/models/submodels/v1/evidences.py
@@ -7,7 +7,7 @@ from rms.models.v1._enums import DocumentName, FileType
 @dataclass
 class Evidence:
     value: str
-    file_type: Optional[str] = None
+    file_type: Optional[FileType] = None
     document_name: Optional[DocumentName] = None
-    origin_timestamp: Optional[FileType] = None
+    origin_timestamp: Optional[str] = None
     version: Literal["1.0.0"] = "1.0.0"
